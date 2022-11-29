8cb1c46241ab2941e111f293609d8197ea9bdf86
Giving <44300487+NGiving@users.noreply.github.com>
2022-11-29 13:24:29 -0500

    change bcrypt to bcryptjs

--- config/passport.js
+++ config/passport.js
@@ -1,7 +1,6 @@
 const db = require('../models/index')
 const passport = require('passport')
 const LocalStrategy = require('passport-local').Strategy
-const bcrypt = require('bcrypt')
 
 passport.use(new LocalStrategy({
     usernameField: 'email', 
